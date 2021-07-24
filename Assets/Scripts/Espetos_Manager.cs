6a0d38f3c1bc064176e35589358b5ee159f32cba
Ronaldo-Rodrigues <Ronaldo.rodrigjunior@gmail.com>
2021-07-24 03:45:17 -0300

    Desafios Fase 2

---
+++ Assets/Scripts/Espetos_Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Espetos_Manager : MonoBehaviour
+{   
+  
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
