ba44be48e34580b7f47eea35812ed694ea5acf24
Ronaldo-Rodrigues <Ronaldo.rodrigjunior@gmail.com>
2021-07-28 01:52:19 -0300

    Implementação de cenas
    Cenas 2, 3 e 4
    Bombas

---
+++ Assets/Scripts/Bomba_Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomba_Manager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bombaFX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bola"))
+        {
+            Instantiate(bombaFX, new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity); 
+        }
+    }
+}
