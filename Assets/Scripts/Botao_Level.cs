31a05c95030976b56c388f28aaba33f687177de6
Ronaldo-Rodrigues <Ronaldo.rodrigjunior@gmail.com>
2021-07-29 00:29:36 -0300

    Level Manager

---
+++ Assets/Scripts/Botao_Level.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Botao_Level : MonoBehaviour
+{
+    public Text txtBtn;
+    public int desbloqueadoBtn; 
+}
---
+++ Assets/Scripts/Level_Manager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level_Manager : MonoBehaviour
+{
+
+    [System.Serializable]
+     public class Level
+    {
+        public string levelText;
+        public bool autorizado;
+        public int desbloquiado;
+    }
+
+   
+    public GameObject botao;
+    public Transform localBtn;
+    public List<Level> levelList;
+
+    void ListaAdd()
+    {
+        foreach (Level level in levelList)
+        {
+            GameObject btnNovo = Instantiate(botao) as GameObject;
+            Botao_Level btnNew = btnNovo.GetComponent<Botao_Level>();
+            btnNew.txtBtn.text = level.levelText;
+            btnNew.desbloqueadoBtn = level.desbloquiado;
+            btnNew.GetComponent<Button>().interactable = level.autorizado;
+
+            if (PlayerPrefs.GetInt("Level" + btnNew.txtBtn.text) == 1)
+            {
+                level.desbloquiado = 1;
+                level.autorizado = true;
+            }
+            btnNew.desbloqueadoBtn = level.desbloquiado;
+            btnNew.GetComponent<Button>().interactable = level.autorizado;
+
+            btnNovo.transform.SetParent(localBtn, false);
+        }
+    }
+
+
+    void Start()
+    {
+        ListaAdd();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
