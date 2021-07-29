d336ca11cdf47c0147170cba7db884c2efd67702
Ronaldo-Rodrigues <Ronaldo.rodrigjunior@gmail.com>
2021-07-29 02:39:17 -0300

    Moedas Manager implementado

---
+++ Assets/Scripts/MoedasControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoedasControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Bola"))
+        {
+            ScoreManager.instance.ColetaMoedas(10);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public int moedas;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+    public void GameStartScoreM()
+    {
+        if (PlayerPrefs.HasKey("MoedasSave"))
+        {
+            moedas = PlayerPrefs.GetInt("MoedasSave");
+        }
+        else
+        {
+            moedas = 100;
+            PlayerPrefs.SetInt("MoedasSave", moedas);
+        }
+    }
+    public void UpdateScore()
+    {
+        moedas = PlayerPrefs.GetInt("MoedasSave");
+    }
+
+    public void ColetaMoedas(int coin)
+    {
+        moedas += coin;
+        SalvaMoedas(moedas);
+    }
+
+    public void PerdeMoedas(int coin)
+    {
+        moedas -= coin;
+        SalvaMoedas(moedas);
+    }
+
+    public void SalvaMoedas(int coin)
+    {
+        PlayerPrefs.SetInt("MoedasSave", coin);
+    }
+}
