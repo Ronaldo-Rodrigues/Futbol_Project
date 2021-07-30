ecefffe85dd4d04471529a48a450e9af35d8d6ec
Ronaldo-Rodrigues <Ronaldo.rodrigjunior@gmail.com>
2021-07-30 01:20:40 -0300

    UI_Manager implementado

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    public Text pontosUI;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        SceneManager.sceneLoaded += Carrega;
+    }
+    
+    void Carrega (Scene cena, LoadSceneMode modo)
+    {
+        pontosUI = GameObject.Find("pontosUI").GetComponent<Text> (); 
+    }
+    public void UpdateUI()
+    {
+        pontosUI.text = ScoreManager.instance.moedas.ToString();
+    }
+}
