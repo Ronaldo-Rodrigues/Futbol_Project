e643ea05ccaa2172097459df72acc55071c62bc9
Ronaldo-Rodrigues <Ronaldo.rodrigjunior@gmail.com>
2021-08-10 15:32:41 -0300

    Botao loja e outros implementos
    Animação da morte da bola
    botao que vai pra loja
    Ui Manager na fase dos menus

---
+++ Assets/Scripts/CameraSegue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSegue : MonoBehaviour
+{
+    [SerializeField]
+    private Transform objE, objD, bola;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.instance.JogoComecou == true)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/UIManagerLevels.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerLevels : MonoBehaviour
+{
+    [SerializeField]
+    private Text moedasLevel;
+
+    void Start()
+    {
+        moedasLevel.text = PlayerPrefs.GetInt("MoedasSave").ToString();
+    }
+
+}   
