0f316f1978000d1b4986d0599138453e8475abde
Ronaldo-Rodrigues <Ronaldo.rodrigjunior@gmail.com>
2021-08-10 23:31:21 -0300

    Scripts da loja

---
+++ Assets/Scripts/Loja Script/BolasLoja.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BolasLoja : MonoBehaviour
+{
+    public int bolasID;
+    public string nomeSprite;
+    public int bolaPreço;
+    public bool comprou;
+}
---
+++ Assets/Scripts/Loja Script/LojadeBolas.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LojadeBolas : MonoBehaviour
+{
+    public static LojadeBolas instance;
+
+    public List<BolasLoja> bolasList = new List<BolasLoja>();
+
+    public GameObject baseBolaItem;
+
+    public Transform conteudo;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FillList()
+    {
+        foreach(BolasLoja b in bolasList)
+        {
+            GameObject itensBola = Instantiate(baseBolaItem) as GameObject;
+            itensBola.transform.SetParent(conteudo, false);
+            BolasVitrine item = itensBola.GetComponent<BolasVitrine>();
+        }
+    }
+}
